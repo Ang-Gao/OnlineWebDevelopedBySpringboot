56b9d3cf5d788d4008230e5b4ce96e958826a53b
Jason Gao <jason@Jasons-MBP.lan>
2023-03-14 15:49:23 +0400

    Update

--- src/main/java/com/teamd0622/controller/UserProfileController.java
+++ src/main/java/com/teamd0622/controller/UserProfileController.java
@@ -1,12 +1,15 @@
 package com.teamd0622.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserProfileController {
-    @PostMapping
-    public String updateProfileInfo(){
-        return "";
+    @PostMapping("/updateUserDetails/{id}")
+    public String updateProfileInfo(@PathVariable Integer id){
+
+        return "events";
     }
 }
--- src/main/java/com/teamd0622/test/DBTestingController.java
+++ src/main/java/com/teamd0622/test/DBTestingController.java
@@ -5,8 +5,5 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DBTestingController {
-    @GetMapping("/events")
-    public String testPage(){
-        return "events";
-    }
+
 }

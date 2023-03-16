ca64ae67bdb5ecf71b1863e7edef18878324902b
zeanggao <zeang.gao2002@outlook.com>
2023-03-16 22:02:26 +0400

    AJAX TEST

--- src/main/java/com/teamd0622/test/DBTestingController.java
+++ src/main/java/com/teamd0622/test/DBTestingController.java
@@ -1,10 +1,19 @@
 package com.teamd0622.test;
 
-import org.springframework.stereotype.Controller;
+import com.teamd0622.model.entity.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class DBTestingController {
 
-
+    //test ajax
+    @GetMapping("/ajax")
+    @CrossOrigin(value = "http://localhost:8080",allowedHeaders = "*",maxAge = 1800)
+    public UserDetails ajaxAction(){
+        UserDetails ud = new UserDetails();
+        ud.setUsername("AJAX Test");
+        return ud;
+    }
 }
